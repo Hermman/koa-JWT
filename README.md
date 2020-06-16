1909616aa8236a8000c2cb2c688a828dccab94c2
yz_hemingzhou <mingzhou.he@yz-intelligence.com>
2020-06-17 02:01:38 +0800

    feat: koa-json-error第三方中间件处理错误信息

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 
 - 运行时错误, 如:500
 - 逻辑错误, 如: 404, 412, 422
+
+- 使用 koa-json-error 第三方中间件
+  安装: koa-json-error
+  使用 koa-json-error 的默认配置处理错误
+  修改配置使其在生产环境下禁用错误堆栈的返回
+
+- 跨平台设置环境变量: cross-env
+  安装: npm i cross-env --save-dev
--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
+const error = require("koa-json-error");
 const app = new Koa();
 const routing = require("./routes");
 
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (error) {
-    ctx.status = error.status || error.statusCode || 500;
-    ctx.body = {
-      message: error.message,
-    };
-  }
-});
+// 错误处理(1)
+/**
+ * 自己造轮子:编写错误处理中间件
+ * 
+  app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (error) {
+        ctx.status = error.status || error.statusCode || 500;
+        ctx.body = {
+        message: error.message,
+        };
+    }
+    });
+ */
 
+// 错误处理(2)
+// 使用第三方中间件
+app.use(
+  error({
+    postFormat: (err, { stack, ...rest }) =>
+      process.env.NODE_ENV === "production" ? rest : { stack, ...rest },
+  })
+);
 app.use(bodyParser());
 routing(app);
 
